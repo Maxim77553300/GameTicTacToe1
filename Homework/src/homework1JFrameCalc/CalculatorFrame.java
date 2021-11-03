764837672de37809a06c3ddad098759dcf1b036f
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2021-11-03 16:18:49 +0300

    Add files via upload

---
+++ Homework/src/homework1JFrameCalc/CalculatorFrame.java
@@ -0,0 +1,24 @@
+package homework1JFrameCalc;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CalculatorFrame extends JFrame {
+    public CalculatorFrame(){
+        setTitle("Калькулятор - Java");
+        CalculatorPanel panel = new CalculatorPanel();
+
+        add(panel);
+        pack();
+        int width = 350;
+        int height = 350;
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int screenWidth = gd.getDisplayMode().getWidth();
+        int screenHeight = gd.getDisplayMode().getHeight();
+        setBounds(screenWidth / 2 - width / 2, screenHeight / 2 - height / 2, width, height);
+    }
+    }
+
+
+
+
---
+++ Homework/src/homework1JFrameCalc/Main.java
@@ -0,0 +1,16 @@
+package homework1JFrameCalc;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                CalculatorFrame frame = new CalculatorFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
