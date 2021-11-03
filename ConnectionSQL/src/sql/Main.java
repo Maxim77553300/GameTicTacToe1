b0ef6701948bcc1bf0f59311e65e8108572aa042
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2021-11-03 15:33:16 +0300

    Add files via upload

---
+++ ConnectionSQL/src/sql/Main.java
@@ -0,0 +1,25 @@
+package sql;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        // url=jdbc:mysql://localhost:3306/library?serverTimezone=UTC&useSSL=false
+        // url=jdbc:mysql://localhost:3306/library
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "77553300");
+        Statement statement = connection.createStatement();
+        statement.executeQuery("SELECT * FROM user");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM user");
+
+
+        if (connection != null) {
+            System.out.println("Connected to the database test");
+        }
+        while(resultSet.next()){
+            System.out.println(resultSet.getString("login"));
+        }
+
+
+    }
+}
